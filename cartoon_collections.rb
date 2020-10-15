c0c7b74263b73d890c4153e9bba97c11b3babba6
Cassandra Whitty <cassandraallexwhitty@gmail.com>
2020-10-15 20:45:54 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,6 +8,9 @@ def greet_characters(array)
 end
 
 def list_dwarves(array)
+  array.each do |element|
+    puts "#{array[element]}. #{element}"
+  end
   # Use `each_with_index` to enumerate over the provided array
   #
   # Print a numbered list of each element
